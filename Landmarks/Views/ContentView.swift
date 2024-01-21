4e9c3e2e8364f0e4956db4cee7550853c18e18df
Moïse VanKeymeulen <moisevk.dev@gmail.com>
2024-01-21 11:16:52 +0100

    Creating and combining views and building lists and naviagtion part 1

---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,52 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Moïse VanKeymeulen on 20/01/2024.
+//
+
+import SwiftUI
+import SwiftData
+
+struct ContentView: View {
+    @Environment(\.modelContext) private var modelContext
+    @Query private var items: [Item]
+
+    var body: some View {
+        VStack {
+            MapView()
+                .frame(height: 300)
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                HStack {
+                    Text("Joshua Tree National Park")
+                    Spacer()
+                    Text("California")
+                }
+                .font(.subheadline)
+                .foregroundStyle(.secondary)
+                
+                Divider()
+                
+                Text("About Turtle Rock")
+                    .font(.title2)
+                Text("Descriptive text goes here.")
+            }
+            .padding()
+            
+            Spacer()
+        }
+
+    }
+}
+
+#Preview {
+    ContentView()
+        .modelContainer(for: Item.self, inMemory: true)
+}
