772efff749df7ba57a95f2809eb739cb2551eb46
Moïse VanKeymeulen <moisevk.dev@gmail.com>
2024-01-23 21:41:12 +0100

    Finish chapter "Building lists and navigation"

---
+++ Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,29 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Moïse VanKeymeulen on 23/01/2024.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        NavigationSplitView {
+            List(landmarks) { landmark in
+                NavigationLink {
+                    LandmarkDetail(landmark: landmark)
+                } label: {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationTitle("Landmarks")
+        } detail: {
+            Text("Select a landmark")
+        }
+    }
+}
+
+#Preview {
+    LandmarkList()
+}
